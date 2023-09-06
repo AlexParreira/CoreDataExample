17d6a704c086a888fedd358466cc359a1560362d
AlexParreira <alexander.o.parreira@gmail.com>
2023-09-06 11:31:10 -0300

    Initial Commit

---
+++ CoreDataExample/CoreDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataExampleApp.swift
+//  CoreDataExample
+//
+//  Created by Alexander Parreira on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
